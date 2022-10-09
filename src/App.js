3c9098bbce11ef58609ff2823bd4d4a07e9adfa2
Zane Hutchison <61723800+zhutcho@users.noreply.github.com>
2022-10-10 08:07:36 +1000

    Created first component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import PlantList from "./PlantList";
+
 function App() {
-  return null;
+  return (
+    <>
+      <PlantList />
+      <input type="text" />
+      <button>Add Plant</button>
+      <button>Clear Plant List</button>
+      <div>0 total plants</div>
+    </>
+  );
 }
 
 export default App;
